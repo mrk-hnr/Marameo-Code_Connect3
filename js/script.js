2c975dbcc372db0eb07a7a10b007785a2f6f99b0
mrk-hnr <mrk.hnr@hotmail.com>
2024-08-18 20:14:55 +0800

    Created manual carousel component

--- js/script.js
+++ js/script.js
@@ -1,13 +1,61 @@
 // VAR DECLARATION
+let prevSlide = document.getElementById("prev-btn");
+let nextSlide = document.getElementById("next-btn");
+let slides = document.querySelectorAll(".slide");
+let curSlide = 0;
+let maxSlide = slides.length - 1;
 
+let currentPage = Number(document.getElementById("current-page").innerText);
 
-// EVENT LISTENERS
+nextSlide.addEventListener("click", newPage);
+prevSlide.addEventListener("click", backPage);
 
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
 
+nextSlide.addEventListener("click", function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
 
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
 
-// FUNCTIONS
+prevSlide.addEventListener("click", function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
 
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
 
+function newPage() {
+  currentPage += 1;
+  document.getElementById("current-page").innerText = currentPage;
+  console.log(currentPage);
 
+  if (currentPage === 9) {
+    currentPage = 0;
+  }
+}
 
+function backPage() {
+  if (currentPage >= 2) {
+    currentPage -= 1;
+    document.getElementById("current-page").innerText = currentPage;
+    console.log(currentPage);
+  } else {
+    currentPage = 9;
+    document.getElementById("current-page").innerText = currentPage;
+    console.log(currentPage);
+  }
+}
