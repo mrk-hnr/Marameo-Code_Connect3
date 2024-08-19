c01d5e0b7aff76e49cd7858f45a3699b741303af
mrk-hnr <mrk.hnr@hotmail.com>
2024-08-19 18:27:46 +0800

    Added header-nav component into index, restructured css for nav and burger

--- js/script.js
+++ js/script.js
@@ -1,63 +1,78 @@
-// VAR DECLARATION
-let prevSlide = document.getElementById("prev-btn");
-let nextSlide = document.getElementById("next-btn");
-let slides = document.querySelectorAll(".slide");
-let curSlide = 0;
-let maxSlide = slides.length - 1;
+// DECLARATION
+let burgerMenu = document.getElementById("burger-menu");
+let overlay = document.getElementById("menu");
+// let prevSlide = document.getElementById("prev-btn");
+// let nextSlide = document.getElementById("next-btn");
+// let slides = document.querySelectorAll(".slide");
+// let curSlide = 0;
+// let maxSlide = slides.length - 1;
 
-let currentPage = Number(document.getElementById("current-page").innerText);
+// let currentPage = Number(document.getElementById("current-page").innerText);
 
-nextSlide.addEventListener("click", newPage);
-prevSlide.addEventListener("click", backPage);
+// EVENT LISTENERS
 
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
+// nextSlide.addEventListener("click", newPage);
+// prevSlide.addEventListener("click", backPage);
+
+// FUNCTIONS
+
+// Func BURGER
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
 });
 
-nextSlide.addEventListener("click", function () {
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+// Func CAROUSEL MANUAL
+// slides.forEach((slide, indx) => {
+//   slide.style.transform = `translateX(${indx * 100}%)`;
+// });
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+// nextSlide.addEventListener("click", function () {
+//   if (curSlide === maxSlide) {
+//     curSlide = 0;
+//   } else {
+//     curSlide++;
+//   }
 
-prevSlide.addEventListener("click", function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+// prevSlide.addEventListener("click", function () {
+//   if (curSlide === 0) {
+//     curSlide = maxSlide;
+//   } else {
+//     curSlide--;
+//   }
 
-function newPage() {
-  if (currentPage == 9) {
-    currentPage = 1;
-    console.log(currentPage);
-    document.getElementById("current-page").innerText = currentPage;
-  } else {
-    currentPage += 1;
-    document.getElementById("current-page").innerText = currentPage;
-    console.log(currentPage);
-  }
-}
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
 
-function backPage() {
-  if (currentPage >= 2) {
-    currentPage -= 1;
-    document.getElementById("current-page").innerText = currentPage;
-    console.log(currentPage);
-  } else if (currentPage == 1) {
-    currentPage = 9;
-    document.getElementById("current-page").innerText = currentPage;
-    console.log(currentPage);
-  }
-}
+// Func CAROUSEL MANUAL PAGE
+
+// function newPage() {
+//   if (currentPage == 9) {
+//     currentPage = 1;
+//     console.log(currentPage);
+//     document.getElementById("current-page").innerText = currentPage;
+//   } else {
+//     currentPage += 1;
+//     document.getElementById("current-page").innerText = currentPage;
+//     console.log(currentPage);
+//   }
+// }
+
+// function backPage() {
+//   if (currentPage >= 2) {
+//     currentPage -= 1;
+//     document.getElementById("current-page").innerText = currentPage;
+//     console.log(currentPage);
+//   } else if (currentPage == 1) {
+//     currentPage = 9;
+//     document.getElementById("current-page").innerText = currentPage;
+//     console.log(currentPage);
+//   }
+// }
